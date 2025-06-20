57b760b57fcc2dcb96e5896de981a83cccdd7691
Nicha <nicha.wilanan@gmail.com>
2025-06-20 11:46:16 +0100

    chore: remove comment

--- api/generate_story.py
+++ api/generate_story.py
@@ -1,10 +1,9 @@
 import logging
 import asyncio
 
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status 
 from pydantic import BaseModel
 
-from services.rag import chroma_client
 from services.llm_generate_story import llm_generate_story
 
 router = APIRouter()
@@ -17,19 +16,12 @@ class Agency(BaseModel):
 
 @router.post("/generate-story/{session_id}", status_code=status.HTTP_202_ACCEPTED)
 async def generate_story(
-    response: Response,
     session_id: str, 
     agency: Agency):
 
     logging.info("Generating story..")
     logging.info(session_id)
     logging.info(agency)
-
-    # try:
-    #     chroma_client.get_collection(name=session_id)
-    # except:
-    #     response.status_code = status.HTTP_404_NOT_FOUND
-    #     return {"error": f"The chroma vector for session_id: {session_id} is not found"}
 
     asyncio.create_task(llm_generate_story(
             session_id, 
